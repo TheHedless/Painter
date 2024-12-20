4c2a098be39b4ac534535ad7e94b43fe668ebe2f
cakes <cakes@haiiro.moe>
2024-12-20 18:09:37 +0100

    fix linking problems on NixOS

--- nix/modules/devshell.nix
+++ nix/modules/devshell.nix
@@ -1,18 +1,30 @@
 { inputs, ... }:
 {
-  perSystem = { config, self', pkgs, lib, ... }: {
-    devShells.default = pkgs.mkShell {
-      name = "Painter-shell";
-      inputsFrom = [
-        self'.devShells.rust
-        config.pre-commit.devShell # See ./nix/modules/pre-commit.nix
+  perSystem = { config, self', pkgs, lib, ... }:
+    let
+      libPath = with pkgs; lib.makeLibraryPath [
+        libGL
+        libxkbcommon
+        wayland
       ];
-      packages = with pkgs; [
-        just
-        nixd # Nix language server
-        bacon
-        config.process-compose.cargo-doc-live.outputs.package
-      ];
+    in
+    {
+      devShells.default = pkgs.mkShell {
+        name = "Painter-shell";
+        inputsFrom = [
+          self'.devShells.rust
+          config.pre-commit.devShell # See ./nix/modules/pre-commit.nix
+        ];
+        packages = with pkgs; [
+          just
+          nixd # Nix language server
+          bacon
+          config.process-compose.cargo-doc-live.outputs.package
+        ];
+
+        RUST_LOG = "debug";
+        RUST_SRC_PATH = "${pkgs.rust.packages.stable.rustPlatform.rustLibSrc}";
+        LD_LIBRARY_PATH = libPath;
+      };
     };
-  };
 }

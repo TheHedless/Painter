deac38b22076c66e8d01b32a245fd79dcb9559d9
TheHedless <csendesdavid76@gmail.com>
2025-01-19 15:45:46 +0100

    Serialization finally works. Optimized node_circles

--- src/main.rs
+++ src/main.rs
@@ -12,26 +12,28 @@ fn main() -> Result<(), eframe::Error> {
         "Painter",
         eframe::NativeOptions::default(),
         Box::new(|_cc| {
-            Ok(Box::<MyApp>::default())
+            Ok(Box::<DrawnShape>::default())
         }),
     )
 }
-//#[derive(Debug, Serialize, Deserialize)]
-struct MyApp {
+#[derive(Debug, Serialize, Deserialize)]
+struct DrawnShape {
     stroke: Stroke,
     node: Vec<Pos2>,
     fill: Color32,
-    point_count: usize,
     filename: String,
+    #[serde(skip)]
     io_status: String,
 }
-impl Default for MyApp {
+impl Default for DrawnShape {
     fn default() -> Self {
         Self {
             stroke: Stroke::new(1.0, Color32::from_rgb(100, 100, 100)),
-            node: Vec::from([Pos2::new(100.0, 100.0), Pos2::new(100.0, 200.0), Pos2::new(50.0, 150.0)]),
+            node: Vec::from([
+                Pos2::new(100.0, 100.0),
+                Pos2::new(100.0, 200.0),
+                Pos2::new(50.0, 150.0)]),
             fill: Color32::from_rgb(50, 50, 50),
-            point_count: 3,
             filename: "".to_string(),
             io_status: "".to_string(),
         }
@@ -39,7 +41,7 @@ impl Default for MyApp {
 }
 
 
-impl MyApp {
+impl DrawnShape {
     fn ui_controls(&mut self, ui: &mut egui::Ui) {
         //control options like line colors
         Grid::new("colors").spacing([5.0, 10.0]).show(ui, |ui| {
@@ -53,11 +55,9 @@ impl MyApp {
             let remove_element = ui.button("Remove Node");
             if add_element.clicked() {
                 self.node.push(Pos2::new(50.0, 100.0));
-                self.point_count += 1
             }
             if remove_element.clicked() {
                 self.node.pop();
-                self.point_count -= 1
             }
         });
     }
@@ -75,40 +75,33 @@ impl MyApp {
             .to_vec()
             .iter_mut()
             .enumerate()
-            .take(self.point_count)
             .map(|(_i, point)| {
-                *point = to_screen.from().clamp(*point);
-                to_screen.transform_pos(*point)
-
+                to_screen.transform_pos(to_screen.from().clamp(*point))
             })
             .collect();
         // dragable circles
         // using node_centers would save us point_in_screen having to be calculated twice
         // however drag logic breaks if it is used
+        for (i, node) in self.node.iter_mut().enumerate() {
+            let size = Vec2::splat(8.0);
+            //gets the points as uniques
+            let point_in_screen = to_screen.transform_pos(*node);
+            let point_rect = Rect::from_center_size(point_in_screen, size);
+            let point_id = response.id.with(i);
+            //drag logic
+            let point_response = ui.interact(point_rect, point_id, Sense::drag());
+            *node = to_screen.from().clamp(*node + point_response.drag_delta());
+        }
         let node_circles: Vec<Shape> = self
             .node
-            .to_vec()
-            .iter_mut()
-            .enumerate()
-            .take(self.point_count)
-            .map(|(i, point)| {
-                let size = Vec2::splat(8.0);
-                //gets the points as uniques
-                let mut point_in_screen = to_screen.transform_pos(*point);
-                let point_rect = Rect::from_center_size(point_in_screen, size);
-                let point_id = response.id.with(i);
-                //drag logic
-                let point_response = ui.interact(point_rect, point_id, Sense::drag());
-                *point += point_response.drag_delta();
-                *point = to_screen.from().clamp(*point);
-                point_in_screen = to_screen.transform_pos(*point);
-                self.node[i] = *point;
-
-                let stroke = ui.style().interact(&point_response).fg_stroke;
-                Shape::circle_stroke(point_in_screen, 4.0, stroke)
+            .iter()
+            .map(|point| {
+                Shape::circle_stroke(
+                    to_screen.transform_pos(*point),
+                    4.0,
+                    Stroke::new(1.0, Color32::from_rgb(255, 255, 255)))
             })
             .collect();
-
         painter.add(PathShape::convex_polygon(node_centers, self.fill, self.stroke));
         // convex_polygon tries to fill from the origin node [0]
         // concave shapes are not fully supported because of this
@@ -124,7 +117,7 @@ impl MyApp {
             let save_button = ui.button("Save");
             let load_button = ui.button("Load");
 
-            let mut named = false;
+            let named;
             if self.filename.is_empty() {
                 named = false
             } else {
@@ -133,12 +126,10 @@ impl MyApp {
 
             if !named { self.io_status = "".to_string(); }
             if save_button.clicked() && named {
-                /*
-                                //add save feature
-                                let file = File::create(self.filename.clone()+".json").unwrap();
-                                let mut writer= BufWriter::new(file);
-                                serde_json::to_writer(&mut writer, &self).expect("write to file failed");
-                                writer.flush().expect("flush failed");*/
+                let file = File::create(self.filename.clone() + ".json").unwrap();
+                let mut writer = BufWriter::new(file);
+                serde_json::to_writer(&mut writer, &self).expect("write to file failed");
+                writer.flush().expect("flush failed");
                 self.io_status = "Saved successfully".to_string();
             }
             if load_button.clicked() && named {
@@ -150,7 +141,7 @@ impl MyApp {
         });
     }
 }
-impl eframe::App for MyApp {
+impl eframe::App for DrawnShape {
     fn update(&mut self, ctx: &egui::Context, _frame: &mut eframe::Frame) {
         egui::CentralPanel::default().show(ctx, |ui| {
             self.ui_controls(ui);
